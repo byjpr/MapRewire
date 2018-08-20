cdd237431792be1b5835e26c4c880921b5a3af3a
Jordan Parker <letsgetjordan@gmail.com>
2018-08-21 00:42:37 +0100

    Base for tests

--- test/map_rewire_test.exs
+++ test/map_rewire_test.exs
@@ -1,8 +1,20 @@
 defmodule MapRewireTest do
   use ExUnit.Case
+  use MapRewire
+
   doctest MapRewire
 
-  test "greets the world" do
-    assert MapRewire.hello() == :world
+  test "1" do
+    assert %{"samplekey" => "samplevalue"} <~> "samplekey=>name" == [%{"name" => "samplevalue"}]
+  end
+
+  test "2" do
+    assert(
+      %{"samplekey" => "samplevalue", "second" => "whoo"} <~> ["samplekey=>name", "second=>third"] ==
+        [
+          %{"name" => "samplevalue", "second" => "whoo"},
+          %{"samplekey" => "samplevalue", "third" => "whoo"}
+        ]
+    )
   end
 end
