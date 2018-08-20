818468a689a7d5deb8d634141cd375cca34d855f
Jordan Parker <letsgetjordan@gmail.com>
2018-08-20 20:27:45 +0100

    Base Mix

--- mix.exs
+++ mix.exs
@@ -6,7 +6,10 @@ defmodule MapRewire.MixProject do
       app: :map_rewire,
       version: "0.1.0",
       elixir: "~> 1.6",
+      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
@@ -21,8 +24,23 @@ defmodule MapRewire.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, "~> 0.18", only: :dev}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+    ]
+  end
+
+  defp description do
+    """
+    Complete syntactic sugar to rekey maps.
+    """
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["Jordan Parker"],
+      links: %{"GitHub" => "https://github.com/byjord/MapRewire"}
     ]
   end
 end
