41838908e11274627655f4450081b1b5f2fc979c
Jordan Parker <letsgetjordan@gmail.com>
2018-08-23 11:21:35 +0100

    :zap: Initial speed runner

---
+++ test/speed_runner.ex
@@ -0,0 +1,20 @@
+defmodule SpeedRunner do
+  import ExProf.Macro
+
+  @data SampleData.sample()
+
+  @doc "analyze with profile macro"
+  def do_analyze do
+    content = @data[:content][:fake_shopify_product_one]
+    transforms = @data[:transforms][:fake_shopify_product_one]
+
+    profile do
+      content <~> transforms
+    end
+  end
+
+  @doc "get analysis records and sum them up"
+  def run do
+    do_analyze
+  end
+end
